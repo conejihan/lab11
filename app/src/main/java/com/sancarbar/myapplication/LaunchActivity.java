75b1c8ccb7613352e87ec7b4e5b64b0648a681a5
conejihan <medinanicolas054@gmail.com>
2021-04-29 11:20:37 -0500

    Lab11

---
+++ app/src/main/java/com/sancarbar/myapplication/LaunchActivity.java
@@ -0,0 +1,28 @@
+package com.sancarbar.myapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LaunchActivity
+        extends AppCompatActivity
+{
+
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
+    @Override
+    protected void onCreate( @Nullable Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        SharedPreferences sharedPref =
+                getSharedPreferences( getString( R.string.preference_file_key ), Context.MODE_PRIVATE );
+
+        if(sharedPref.contains(TOKEN_KEY)){
+            //TODO go to MainActivity
+        }else{
+            //TODO go to LoginActivity
+        }
+    }
+}
